c73ee43af2c250392aa78bee05949e82af832db9
Ishita <dwivedii525@gmail.com>
2023-08-29 11:26:50 +0530

    bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
+    
+    for i in range(n):
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+# Example usage
+arr = [64, 34, 25, 12, 22, 11, 90]
+bubble_sort(arr)
+
+print("Sorted array:")
+for num in arr:
+    print(num, end=" ")
